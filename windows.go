34f088ad787e6e5c063933c7afdff783353fb8b0
SirSAC <80664762+SirSAC@users.noreply.github.com>
2021-03-15 07:06:09 +0200

    Add files via upload

---
+++ windows.go
@@ -0,0 +1,70 @@
+// Made by SirSAC for Network.
+package main
+//
+import (
+	. "fmt"
+	"log"
+	. "net"
+	"os/exec"
+	"runtime"
+)
+//
+func handle_internet(local_connection Conn, address string, pipe_size int, try_count int, no_delay bool, keep_alive bool, serial bool) {
+	remote_address, _ := ResolveTCPAddr("tcp", address)
+	load_balancer := get_load_balancer(serial)
+	local_address, _ := ResolveTCPAddr("tcp", load_balancer.address)
+	remote_connection, err := DialTCP("tcp", local_address, remote_address)
+	if err != nil {
+		try_again := 0
+		for try_again < try_count {
+			load_balancer := get_load_balancer(serial)
+			local_address, _ := ResolveTCPAddr("tcp", load_balancer.address)
+			remote_connection, err := DialTCP("tcp", local_address, remote_address)
+			if err == nil {
+				go handle_proxy(local_connection, remote_connection, load_balancer.address, address, pipe_size, no_delay, keep_alive)
+				return
+			}
+			try_again++
+		}
+		log.Println(string(COLOR_YELLOW), "[!]", address, "-=>", load_balancer.address, Sprintf("{%s}", err), string(COLOR_RESET))
+		local_connection.Write([]byte {5, NETWORK_UNREACHABLE, 0, 1, 0, 0, 0, 0, 0, 0})
+		local_connection.Close()
+		runtime.Goexit()
+	}
+	go handle_proxy(local_connection, remote_connection, load_balancer.address, address, pipe_size, no_delay, keep_alive)
+}
+//
+func execute_command() {
+	if runtime.GOOS == "windows" {
+		exec.Command("powershell.exe", `(wmic.exe process where "name='GoDispatchProxy.bin'" call setpriority "realtime")`).Run()
+		exec.Command("powershell.exe", `(net.exe stop /y "RemoteAccess")`).Run()
+		exec.Command("powershell.exe", `(sc.exe config "RemoteAccess" start="disabled")`).Run()
+		exec.Command("powershell.exe", `(netsh.exe interface ipv6 set interface interface="1" metric="1" store="active")`).Run()
+		exec.Command("powershell.exe", `(netsh.exe interface ipv6 set subinterface interface="1" mtu="1280" store="active")`).Run()
+		exec.Command("powershell.exe", `(netsh.exe interface ipv6 set address interface="1" type="anycast" store="active")`).Run()
+		exec.Command("powershell.exe", `(netsh.exe interface ipv6 set address interface="1" address="::1" type="unicast" validlifetime="infinite" preferredlifetime="infinite" store="active")`).Run()
+		exec.Command("powershell.exe", `(netsh.exe interface ipv6 set dnsservers name="1" source="dhcp" register="both" validate="yes")`).Run()
+		exec.Command("powershell.exe", `(netsh.exe interface ipv4 set interface interface="1" metric="1" store="active")`).Run()
+		exec.Command("powershell.exe", `(netsh.exe interface ipv4 set subinterface interface="1" mtu="576" store="active")`).Run()
+		exec.Command("powershell.exe", `(netsh.exe interface ipv4 set address name="1" source="dhcp" type="anycast" store="active")`).Run()
+		exec.Command("powershell.exe", `(netsh.exe interface ipv4 set address name="1" source="static" address="127.0.0.1" mask="255.255.255.255" gwmetric="1" type="unicast" store="active")`).Run()
+		exec.Command("powershell.exe", `(netsh.exe interface ipv4 set dnsservers name="1" source="dhcp" register="both" validate="yes")`).Run()
+		exec.Command("powershell.exe", `(netsh.exe interface tcp set global ecncapability="enabled")`).Run()
+		exec.Command("powershell.exe", `(netsh.exe interface tcp set global fastopen="enabled")`).Run()
+		exec.Command("powershell.exe", `(netsh.exe interface tcp set global timestamps="enabled")`).Run()
+		exec.Command("powershell.exe", `(netsh.exe interface portproxy set v4tov6 listenport="8118" connectaddress="::1" connectport="8118" listenaddress="127.0.0.1")`).Run()
+		exec.Command("powershell.exe", `(netsh.exe interface portproxy set v4tov6 listenport="1080" connectaddress="::1" connectport="1080" listenaddress="127.0.0.1")`).Run()
+		exec.Command("powershell.exe", `(netsh.exe lan set autoconfig enabled="yes" interface="*")`).Run()
+		exec.Command("powershell.exe", `(netsh.exe lan set allowexplicitcreds allow="yes")`).Run()
+		exec.Command("powershell.exe", `(netsh.exe wlan set randomization enabled="yes" interface="*")`).Run()
+		exec.Command("powershell.exe", `(netsh.exe wlan set autoconfig enabled="yes" interface="*")`).Run()
+		exec.Command("powershell.exe", `(netsh.exe wlan set hostednetwork mode="allow")`).Run()
+		exec.Command("powershell.exe", `(netsh.exe wlan set blockednetworks display="show")`).Run()
+		exec.Command("powershell.exe", `(netsh.exe wlan set allowexplicitcreds allow="yes")`).Run()
+		exec.Command("powershell.exe", `(netsh.exe winhttp set proxy proxy-server="[::1]:8118")`).Run()
+		exec.Command("powershell.exe", `(netsh.exe http add iplisten ipaddress="::1")`).Run()
+		exec.Command("powershell.exe", `(netsh.exe http add iplisten ipaddress="127.0.0.1")`).Run()
+		exec.Command("powershell.exe", `(ipconfig.exe /registerdns)`).Run()
+		exec.Command("powershell.exe", `(ipconfig.exe /flushdns)`).Run()
+	}
+}
