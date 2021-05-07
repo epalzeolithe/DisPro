5a3befaeeba84ec6100a57f9ca5747ccc7021752
Sergiu Alexandru Capatina <sergiu.alexandru.capatina@hotmail.com>
2021-05-07 12:41:47 +0300

    Update darwin.go
    Making variables more explicit and making better execution order of code.

--- darwin.go
+++ darwin.go
@@ -13,17 +13,17 @@ func handle_internet(local_connection Conn, target_address string, processor_thr
 		sync_group.Add(processor_thread)
 	}
 	load_balancer := get_load_balancer(serial_order)
-	local_address, _ := ResolveTCPAddr("tcp", load_balancer.address)
+	local_address, _ := ResolveTCPAddr("tcp", load_balancer.interface_address)
 	remote_address, _ := ResolveTCPAddr("tcp", target_address)
 	remote_connection, err := DialTCP("tcp", local_address, remote_address)
 	if err != nil {
 		try_again := 0
 		for try_again < try_count {
 			load_balancer := get_load_balancer(serial_order)
-			local_address, _ := ResolveTCPAddr("tcp", load_balancer.address)
+			local_address, _ := ResolveTCPAddr("tcp", load_balancer.interface_address)
 			remote_connection, err := DialTCP("tcp", local_address, remote_address)
 			if err == nil {
-				go handle_proxy(local_connection, remote_connection, load_balancer.address, target_address, pipe_size, delay_protocol, keep_alive)
+				go handle_proxy(local_connection, remote_connection, load_balancer.interface_address, target_address, pipe_size, delay_protocol, keep_alive)
 				if serial_order == true {
 					defer sync_group.Wait()
 				}
@@ -31,7 +31,7 @@ func handle_internet(local_connection Conn, target_address string, processor_thr
 			}
 			try_again++
 		}
-		log.Println(string(COLOR_YELLOW), "[!]", target_address, "-=>", load_balancer.address, Sprintf("{%s}", err), string(COLOR_RESET))
+		log.Println(string(COLOR_YELLOW), "[!]", target_address, "-=>", load_balancer.interface_address, Sprintf("{%s}", err), string(COLOR_RESET))
 		local_connection.Write([]byte {5, NETWORK_UNREACHABLE, 0, 1, 0, 0, 0, 0, 0, 0})
 		local_connection.Close()
 		if serial_order == true {
@@ -40,7 +40,7 @@ func handle_internet(local_connection Conn, target_address string, processor_thr
 		}
 		runtime.Goexit()
 	}
-	go handle_proxy(local_connection, remote_connection, load_balancer.address, target_address, pipe_size, delay_protocol, keep_alive)
+	go handle_proxy(local_connection, remote_connection, load_balancer.interface_address, target_address, pipe_size, delay_protocol, keep_alive)
 	if serial_order == true {
 		defer sync_group.Wait()
 	}
