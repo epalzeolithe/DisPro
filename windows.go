0b6d979616cd2588f7cb2ccdd286af5067ccdfa3
Sergiu Alexandru Capatina <sergiu.alexandru.capatina@hotmail.com>
2021-05-15 19:48:34 +0300

    Update windows.go
    Big changes with improvements and some fixes.

--- windows.go
+++ windows.go
@@ -2,29 +2,30 @@
 package main
 //
 import (
-	. "fmt"
+	. "context"
 	"log"
 	. "net"
 	"os/exec"
 	"runtime"
 )
 //
-func handle_internet(local_connection Conn, target_address string, processor_thread int, pipe_size int, try_count int, delay_protocol bool, keep_alive bool, serial_order bool) {
+func handle_internet(local_connection Conn, target_address string, processor_thread int, pipe_size int, try_count int, keep_alive bool, serial_order bool) {
 	if serial_order == true {
 		sync_group.Add(processor_thread)
 	}
 	load_balancer := get_load_balancer(serial_order)
 	local_address, _ := ResolveTCPAddr("tcp", load_balancer.interface_address)
-	remote_address, _ := ResolveTCPAddr("tcp", target_address)
-	remote_connection, err := DialTCP("tcp", local_address, remote_address)
+	network_dialer.LocalAddr = local_address
+	remote_connection, err := network_dialer.DialContext(Background(), "tcp", target_address)
 	if err != nil {
 		try_again := 0
 		for try_again < try_count {
 			load_balancer := get_load_balancer(serial_order)
 			local_address, _ := ResolveTCPAddr("tcp", load_balancer.interface_address)
-			remote_connection, err := DialTCP("tcp", local_address, remote_address)
+			network_dialer.LocalAddr = local_address
+			remote_connection, err := network_dialer.DialContext(Background(), "tcp", target_address)
 			if err == nil {
-				go handle_proxy(local_connection, remote_connection, load_balancer.interface_address, target_address, pipe_size, delay_protocol, keep_alive)
+				go handle_proxy(local_connection, remote_connection, local_address, target_address, pipe_size, keep_alive)
 				if serial_order == true {
 					defer sync_group.Wait()
 				}
@@ -32,7 +33,7 @@ func handle_internet(local_connection Conn, target_address string, processor_thr
 			}
 			try_again++
 		}
-		log.Println(string(COLOR_YELLOW), "[!]", target_address, "-=>", load_balancer.interface_address, Sprintf("{%s}", err), string(COLOR_RESET))
+		log.Println(string(COLOR_YELLOW), "[!]", local_address, "<=>", target_address, string(COLOR_RESET))
 		local_connection.Write([]byte {5, NETWORK_UNREACHABLE, 0, 1, 0, 0, 0, 0, 0, 0})
 		local_connection.Close()
 		if serial_order == true {
@@ -41,14 +42,14 @@ func handle_internet(local_connection Conn, target_address string, processor_thr
 		}
 		runtime.Goexit()
 	}
-	go handle_proxy(local_connection, remote_connection, load_balancer.interface_address, target_address, pipe_size, delay_protocol, keep_alive)
+	go handle_proxy(local_connection, remote_connection, local_address, target_address, pipe_size, keep_alive)
 	if serial_order == true {
 		defer sync_group.Wait()
 	}
 }
 //
 func execute_command(mtu_size string, option_setting bool) {
-	exec.Command("cmd.exe", "/c", "wmic.exe process where 'name='DisPro.bin'' call setpriority realtime").Run()
+	exec.Command("cmd.exe", "/c", "wmic.exe process where 'name='DisPro.exe'' call setpriority realtime").Run()
 	exec.Command("cmd.exe", "/c", "net.exe stop /y RemoteAccess").Run()
 	exec.Command("cmd.exe", "/c", "netsh.exe interface ipv6 set interface interface=1 metric=1 store=active").Run()
 	exec.Command("cmd.exe", "/c", "netsh.exe interface ipv6 set subinterface interface=1 mtu=", mtu_size, "store=active").Run()
