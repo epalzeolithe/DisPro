6b7b006bf6bd91ef3bf0ff9294301a96789e465d
Sergiu Alexandru Capatina <sergiu.alexandru.capatina@hotmail.com>
2021-05-15 19:43:45 +0300

    Update linux.go
    Big changes with improvements and some fixes.

--- linux.go
+++ linux.go
@@ -2,7 +2,7 @@
 package main
 //
 import (
-	. "fmt"
+	. "context"
 	"log"
 	. "net"
 	"os/exec"
@@ -10,48 +10,39 @@ import (
 	"syscall"
 )
 //
-func handle_bind(local_connection Conn, remote_connection Conn, balancer_address string, target_address string, pipe_size int, delay_protocol bool, keep_alive bool) {
-	local_connection.Write([]byte {5, REQUEST_GRANTED, 0, 1, 0, 0, 0, 0, 0, 0})
-	log.Println(string(COLOR_BLUE), "[*]", target_address, "-=>", balancer_address, string(COLOR_RESET))
-	go handle_pipe(local_connection, remote_connection, pipe_size, keep_alive)
-	go handle_pipe(remote_connection, local_connection, pipe_size, keep_alive)
-}
-//
-func handle_internet(local_connection Conn, target_address string, processor_thread int, pipe_size int, try_count int, delay_protocol bool, keep_alive bool, serial_order bool) {
+func handle_internet(local_connection Conn, target_address string, processor_thread int, pipe_size int, try_count int, keep_alive bool, serial_order bool) {
 	if serial_order == true {
 		sync_group.Add(processor_thread)
 	}
 	load_balancer := get_load_balancer(serial_order)
 	local_address, _ := ResolveTCPAddr("tcp", load_balancer.interface_address)
-	network_dialer := Dialer {
-		LocalAddr: local_address, Control: func(network_protocol string, remote_address string, c syscall.RawConn) (err error) {
-			return c.Control(func(fd uintptr) {
-				err := syscall.BindToDevice(int(fd), load_balancer.interface_name)
-				if err != nil {
-					log.Println(string(COLOR_YELLOW), "[!] Couldn't bind to interface", load_balancer.interface_name, string(COLOR_RESET))
-				}
-			})
-		},
+	network_dialer.LocalAddr = local_address
+	network_dialer.Control = func(network_protocol string, remote_address string, network_control syscall.RawConn) (err error) {
+		return network_control.Control(func(fd uintptr) {
+			err := syscall.BindToDevice(int(fd), load_balancer.interface_name)
+			if err != nil {
+				log.Println(string(COLOR_YELLOW), "[!] Could not bind to interface", load_balancer.interface_name, string(COLOR_RESET))
+			}
+		})
 	}
-	remote_connection, err := network_dialer.Dial("tcp", target_address)
+	remote_connection, err := network_dialer.DialContext(Background(), "tcp", target_address)
 	if err != nil {
 		try_again := 0
 		for try_again < try_count {
 			load_balancer := get_load_balancer(serial_order)
 			local_address, _ := ResolveTCPAddr("tcp", load_balancer.interface_address)
-			network_dialer := Dialer {
-				LocalAddr: local_address, Control: func(network_protocol string, remote_address string, c syscall.RawConn) (err error) {
-					return c.Control(func(fd uintptr) {
-						err := syscall.BindToDevice(int(fd), load_balancer.interface_name)
-						if err != nil {
-							log.Println(string(COLOR_YELLOW), "[!] Couldn't bind to interface", load_balancer.interface_name, string(COLOR_RESET))
-						}
-					})
-				},
+			network_dialer.LocalAddr = local_address
+			network_dialer.Control = func(network_protocol string, remote_address string, network_control syscall.RawConn) (err error) {
+				return network_control.Control(func(fd uintptr) {
+					err := syscall.BindToDevice(int(fd), load_balancer.interface_name)
+					if err != nil {
+						log.Println(string(COLOR_YELLOW), "[!] Could not bind to interface", load_balancer.interface_name, string(COLOR_RESET))
+					}
+				})
 			}
-			remote_connection, err := network_dialer.Dial("tcp", target_address)
+			remote_connection, err := network_dialer.DialContext(Background(), "tcp", target_address)
 			if err == nil {
-				go handle_bind(local_connection, remote_connection, load_balancer.interface_address, target_address, pipe_size, delay_protocol, keep_alive)
+				go handle_proxy(local_connection, remote_connection, local_address, target_address, pipe_size, keep_alive)
 				if serial_order == true {
 					defer sync_group.Wait()
 				}
@@ -59,7 +50,7 @@ func handle_internet(local_connection Conn, target_address string, processor_thr
 			}
 			try_again++
 		}
-		log.Println(string(COLOR_YELLOW), "[!]", target_address, "-=>", load_balancer.interface_address, Sprintf("{%s}", err), string(COLOR_RESET))
+		log.Println(string(COLOR_YELLOW), "[!]", local_address, "<=>", target_address, string(COLOR_RESET))
 		local_connection.Write([]byte {5, NETWORK_UNREACHABLE, 0, 1, 0, 0, 0, 0, 0, 0})
 		local_connection.Close()
 		if serial_order == true {
@@ -68,7 +59,7 @@ func handle_internet(local_connection Conn, target_address string, processor_thr
 		}
 		runtime.Goexit()
 	}
-	go handle_bind(local_connection, remote_connection, load_balancer.interface_address, target_address, pipe_size, delay_protocol, keep_alive)
+	go handle_proxy(local_connection, remote_connection, local_address, target_address, pipe_size, keep_alive)
 	if serial_order == true {
 		defer sync_group.Wait()
 	}
