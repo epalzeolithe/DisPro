8b217024f34925798e309f9959372c0229366bdf
Sergiu Alexandru Capatina <sergiu.alexandru.capatina@hotmail.com>
2021-06-11 19:20:37 +0300

    Update main.go
    Big changes with improvements and some fixes.

--- main.go
+++ main.go
@@ -4,8 +4,8 @@ package main
 import (
 	"bufio"
 	"bytes"
+	. "context"
 	. "flag"
-	. "fmt"
 	"io"
 	"log"
 	. "net"
@@ -16,6 +16,7 @@ import (
 	. "strconv"
 	"strings"
 	. "sync"
+	"time"
 )
 //
 type struct_balancer struct {
@@ -28,6 +29,8 @@ type struct_balancer struct {
 var (
 	lb_list []struct_balancer
 	lb_index int = 0
+	timeout_verify time.Duration
+	network_dialer Dialer
 	buffer_byte bytes.Buffer
 	sync_group *WaitGroup
 	sync_mutex *Mutex
@@ -61,37 +64,36 @@ func handle_pipe(source_packet Conn, destination_packet Conn, buffer_size int, k
 	buffer_packet := buffer_byte.Bytes()
 	buffer_source := bufio.NewReaderSize(source_packet, buffer_size)
 	buffer_destination := bufio.NewWriterSize(destination_packet, buffer_size)
-	io.CopyBuffer(buffer_destination, buffer_source, buffer_packet)
+	_, err := io.CopyBuffer(buffer_destination, buffer_source, buffer_packet)
+	if err != nil {
+		defer buffer_destination.Flush()
+		runtime.Goexit()
+	}
 }
 //
-func handle_proxy(local_connection Conn, remote_connection *TCPConn, balancer_address string, target_address string, pipe_size int, delay_protocol bool, keep_alive bool) {
-	remote_connection.SetNoDelay(delay_protocol)
-	remote_connection.SetKeepAlive(keep_alive)
-	if keep_alive == false {
-		remote_connection.SetLinger(0)
-	}
+func handle_proxy(local_connection Conn, remote_connection Conn, balancer_address Addr, target_address string, pipe_size int, keep_alive bool) {
 	local_connection.Write([]byte {5, REQUEST_GRANTED, 0, 1, 0, 0, 0, 0, 0, 0})
-	log.Println(string(COLOR_BLUE), "[*]", target_address, "-=>", balancer_address, string(COLOR_RESET))
+	log.Println(string(COLOR_BLUE), "[*]", balancer_address, "<=>", target_address, string(COLOR_RESET))
 	go handle_pipe(local_connection, remote_connection, pipe_size, keep_alive)
 	go handle_pipe(remote_connection, local_connection, pipe_size, keep_alive)
 }
 //
-func handle_tunnel(local_connection Conn, processor_thread int, pipe_size int, try_count int, delay_protocol bool, keep_alive bool, serial_order bool) {
+func handle_tunnel(local_connection Conn, processor_thread int, pipe_size int, try_count int, keep_alive bool, serial_order bool) {
 	if serial_order == true {
 		sync_group.Add(processor_thread)
 	}
+	print_message := string("Tunnelled")
 	load_balancer := get_load_balancer(serial_order)
 	remote_address, _ := ResolveTCPAddr("tcp", load_balancer.interface_address)
-	print_message := string("Tunnelled")
-	remote_connection, err := DialTCP("tcp", nil, remote_address)
+	remote_connection, err := network_dialer.DialContext(Background(), "tcp", load_balancer.interface_address)
 	if err != nil {
 		try_again := 0
 		for try_again < try_count {
 			load_balancer := get_load_balancer(serial_order)
 			remote_address, _ := ResolveTCPAddr("tcp", load_balancer.interface_address)
-			remote_connection, err := DialTCP("tcp", nil, remote_address)
+			remote_connection, err := network_dialer.DialContext(Background(), "tcp", load_balancer.interface_address)
 			if err == nil {
-				go handle_proxy(local_connection, remote_connection, load_balancer.interface_address, print_message, pipe_size, delay_protocol, keep_alive)
+				go handle_proxy(local_connection, remote_connection, remote_address, print_message, pipe_size, keep_alive)
 				if serial_order == true {
 					defer sync_group.Wait()
 				}
@@ -99,7 +101,7 @@ func handle_tunnel(local_connection Conn, processor_thread int, pipe_size int, t
 			}
 			try_again++
 		}
-		log.Println(string(COLOR_YELLOW), "[!]", load_balancer.interface_address, Sprintf("{%s}", err), string(COLOR_RESET))
+		log.Println(string(COLOR_YELLOW), "[!]", remote_address, string(COLOR_RESET))
 		local_connection.Write([]byte {5, HOST_UNREACHABLE, 0, 1, 0, 0, 0, 0, 0, 0})
 		local_connection.Close()
 		if serial_order == true {
@@ -108,30 +110,30 @@ func handle_tunnel(local_connection Conn, processor_thread int, pipe_size int, t
 		}
 		runtime.Goexit()
 	}
-	go handle_proxy(local_connection, remote_connection, load_balancer.interface_address, print_message, pipe_size, delay_protocol, keep_alive)
+	go handle_proxy(local_connection, remote_connection, remote_address, print_message, pipe_size, keep_alive)
 	if serial_order == true {
 		defer sync_group.Wait()
 	}
 }
 //
-func detect_interface(network_address string, list_network bool) (string) {
-	if list_network == true {
+func detect_interface(network_address string, network_list bool) (string) {
+	if network_list == true {
 		log.Println(string(COLOR_CYAN), "[-] Listing the available addresses for dispatching", string(COLOR_RESET))
 	}
-	ifaces, _ := Interfaces()
-	for _, iface := range ifaces {
-		if (iface.Flags&FlagUp == FlagUp) && (iface.Flags&FlagLoopback != FlagLoopback) {
-			addrs, _ := iface.Addrs()
+	interface_list, _ := Interfaces()
+	for _, interface_network := range interface_list {
+		if (interface_network.Flags&FlagUp == FlagUp) && (interface_network.Flags&FlagLoopback != FlagLoopback) {
+			addrs, _ := interface_network.Addrs()
 			for _, addr := range addrs {
 				if ipnet, ok := addr.(*IPNet); ok && !ipnet.IP.IsLoopback() {
 					if ipnet.IP.To16() != nil {
-						if list_network == false {
+						if network_list == false {
 							if ipnet.IP.String() == network_address {
-								return iface.Name
+								return interface_network.Name
 							}
 						}
-						if list_network == true {
-							log.Printf("%s [+] %s, IP: %s %s\n", string(COLOR_MAGENTA), iface.Name, ipnet.IP.String(), string(COLOR_RESET))
+						if network_list == true {
+							log.Printf("%s [+] %s, IP: %s %s\n", string(COLOR_MAGENTA), interface_network.Name, ipnet.IP.String(), string(COLOR_RESET))
 						}
 					}
 				}
@@ -141,7 +143,7 @@ func detect_interface(network_address string, list_network bool) (string) {
 	return ""
 }
 //
-func parse_network(argument_network []string, tunnel bool, list_network bool) (mtu_standard int, try_standard int) {
+func parse_network(argument_network []string, tunnel bool, network_list bool) (mtu_standard int, try_standard int) {
 	if len(argument_network) == 0 {
 		log.Fatalln(string(COLOR_RED), "[x] Please specify one or more network addresses", string(COLOR_RESET))
 	}
@@ -158,7 +160,7 @@ func parse_network(argument_network []string, tunnel bool, list_network bool) (m
 		if tunnel == false {
 			network_address = splitted[0]
 			network_port = 0
-			interface_name = detect_interface(network_address, list_network)
+			interface_name = detect_interface(network_address, network_list)
 			if interface_name == "" {
 				log.Fatalln(string(COLOR_RED), "[x] IP address not associated with an interface", network_address, string(COLOR_RESET))
 			}
@@ -185,22 +187,25 @@ func parse_network(argument_network []string, tunnel bool, list_network bool) (m
 			}
 		}
 		log.Printf("%s [i] Load balancer %s: %s, contention ratio: %d %s\n", string(COLOR_GREEN), interface_name, network_address, contention_ratio, string(COLOR_RESET))
-		lb_list[idx] = struct_balancer {interface_address: Sprintf("%s:%d", network_address, network_port), contention_ratio: contention_ratio, interface_name: interface_name, current_connection: 0}
+		interface_address := JoinHostPort(network_address, Itoa(network_port))
+		lb_list[idx] = struct_balancer {interface_address: interface_address, contention_ratio: contention_ratio, interface_name: interface_name, current_connection: 0}
 		mtu_standard = mtu_standard + 1500
 		try_standard = try_standard + contention_ratio
 	}
 	return
 }
 //
-func handle_network(local_connection Conn, processor_thread int, pipe_size int, try_count int, tunnel bool, secure_connection bool, delay_protocol bool, keep_alive bool, serial_order bool) {
+func handle_network(local_connection Conn, processor_thread int, pipe_size int, try_count int, tunnel bool, secure_connection bool, keep_alive bool, serial_order bool) {
 	if tunnel == false {
 		target_address, err := handle_socks(local_connection, secure_connection)
-		if err == nil {
-			go handle_internet(local_connection, target_address, processor_thread, pipe_size, try_count, delay_protocol, keep_alive, serial_order)
+		if err != nil {
+			defer local_connection.Close()
+			runtime.Goexit()
 		}
+		go handle_internet(local_connection, target_address, processor_thread, pipe_size, try_count, keep_alive, serial_order)
 	}
 	if tunnel == true {
-		go handle_tunnel(local_connection, processor_thread, pipe_size, try_count, delay_protocol, keep_alive, serial_order)
+		go handle_tunnel(local_connection, processor_thread, pipe_size, try_count, keep_alive, serial_order)
 	}
 }
 //
@@ -208,6 +213,7 @@ func main() {
 	if runtime.GOOS == "windows" {
 		exec.Command("powershell.exe").Run()
 	}
+	runtime.UnlockOSThread()
 	processor_thread := runtime.NumCPU()
 	var (
 		host = String("host", "::1", "The IP address to listen for SOCKS connection")
@@ -218,11 +224,9 @@ func main() {
 		tunnel = Bool("tunnel", false, "Use tunnel mode (acts as a transparent load balancing proxy)")
 		option = Bool("option", false, "Use option mode (sets the operating system options for maximum potential)")
 		secure = Bool("secure", false, "Use secure mode (acts like using secure ports than usual ones)")
-		delay = Bool("delay", false, "Use delay mode (acts a combining a number of small outgoing messages and sending them all at once)")
-		keep = Bool("keep", false, "Use keep mode (sets whether the operating system should send keep-alive messages on the connection)")
+		keep = Bool("keep", false, "Use keep mode (sets whether the program should keep the connection alive even if it is done)")
 		serial = Bool("serial", false, "Use serial mode (acts to serialize access to function get load balancer)")
 		list = Bool("list", false, "Shows the available addresses for dispatching (non-tunneling mode only)")
-		delay_protocol bool
 	)
 	runtime.GOMAXPROCS(processor_thread * *multiply)
 	SetGCPercent(*percent)
@@ -237,16 +241,14 @@ func main() {
 	if *port < 1 || *port > 65535 {
 		log.Fatalln(string(COLOR_RED), "[x] Invalid port", *port, string(COLOR_RESET))
 	}
-	local_port := Itoa(*port)
-	host_port := JoinHostPort(*host, local_port)
+	host_port := JoinHostPort(*host, Itoa(*port))
 	local_host, err := Listen("tcp", host_port)
 	if err != nil {
 		log.Fatalln(string(COLOR_RED), "[x] Could not start local server on", host_port, string(COLOR_RESET))
 	}
-	mtu_jumbo, try_number := parse_network(Args(), *tunnel, *list)
+	mtu_jumbo, try_count := parse_network(Args(), *tunnel, *list)
 	mtu_size := Itoa(mtu_jumbo)
-	execute_command(mtu_size, *option)
-	try_count := try_number - 1
+	go execute_command(mtu_size, *option)
 	log.Println(string(COLOR_GREEN), "[i] Local server started on", host_port, string(COLOR_RESET))
 	log.Println(string(COLOR_GREEN), "[i] Jumbo size is", mtu_jumbo, string(COLOR_RESET))
 	log.Println(string(COLOR_GREEN), "[i] Multiply thread is", *multiply, string(COLOR_RESET))
@@ -256,22 +258,21 @@ func main() {
 	log.Println(string(COLOR_GREEN), "[i] Tunnel is", *tunnel, string(COLOR_RESET))
 	log.Println(string(COLOR_GREEN), "[i] Option setting is", *option, string(COLOR_RESET))
 	log.Println(string(COLOR_GREEN), "[i] Secure connection is", *secure, string(COLOR_RESET))
-	log.Println(string(COLOR_GREEN), "[i] Delay protocol is", *delay, string(COLOR_RESET))
 	log.Println(string(COLOR_GREEN), "[i] Keep alive is", *keep, string(COLOR_RESET))
 	log.Println(string(COLOR_GREEN), "[i] Serial order is", *serial, string(COLOR_RESET))
-	if *delay == false {
-		delay_protocol = true
-	}
-	if *delay == true {
-		delay_protocol = false
-	}
 	if *serial == true {
 		sync_group = &WaitGroup {}
 		sync_mutex = &Mutex {}
 	}
+	timeout_verify, _ := time.ParseDuration("500ms")
+	timeout_check, _ := time.ParseDuration("250ms")
+	network_dialer.LocalAddr = nil
+	network_dialer.Timeout = timeout_verify
+	network_dialer.FallbackDelay = timeout_check
+	network_dialer.DualStack = true
 	for {
 		local_connection, _ := local_host.Accept()
-		go handle_network(local_connection, processor_thread, *pipe, try_count, *tunnel, *secure, delay_protocol, *keep, *serial)
+		go handle_network(local_connection, processor_thread, *pipe, try_count, *tunnel, *secure, *keep, *serial)
 		if *serial == false {
 			continue
 		}
